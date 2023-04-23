885b6a002d2d9237afecb1fe1d90555dc06cd33b
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-04-23 12:18:26 +0530

    Create Java_Output_Formatting

---
+++ Java_Output_Formatting.java
@@ -0,0 +1,22 @@
+/*Java's System.out.printf function can be used to print formatted output. The purpose of this exercise is to test your understanding of formatting output using printf.
+
+To get you started, a portion of the solution is provided for you in the editor; you must format and print the input to complete the solution.
+*/
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                System.out.printf("%-15s%03d%n",s1,x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
