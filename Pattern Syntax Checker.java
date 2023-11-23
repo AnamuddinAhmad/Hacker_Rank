6b006e6dc5263b570bc97ee73f43d00e37b52f74
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-11-23 21:26:56 +0530

    Update Pattern Syntax Checker.java

--- Pattern Syntax Checker.java
+++ Pattern Syntax Checker.java
@@ -4,21 +4,25 @@ import java.util.regex.*;
 public class Solution
 {
 
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        for(int i=0; i<num; i++) {
-            String s = sc.next();
-            try {
-                Pattern.compile(s);
-                System.out.println("Valid");
-            } catch (PatternSyntaxException e) {
-                System.out.println("Invalid");
-            }
-        }
-        
+  public static void patternChecker(String a){
+    try{
+        Pattern.compile(a);
+        System.out.println("Valid");
+    }
+    catch(Exception e){
+        System.out.println("Invalid");
     }
 }
 
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int tc = sc.nextInt();
+    String a;
+    sc.nextLine();
+    while(tc-->0){
+        a=sc.nextLine();
+        patternChecker(a);
+    }
+    sc.close();
+}
+}
